0bcb385a3c7ca8f29e6234a5f515f2131a195f3f
kamran <mdkamran1755@gmail.com>
2023-11-25 17:09:33 +0530

    first commit

---
+++ routes/users.js
@@ -0,0 +1,42 @@
+// models/User.js
+const mongoose = require('mongoose');
+const plm = require('passport-local-mongoose')
+mongoose.connect("mongodb://127.0.0.1:27017/pinterest")
+
+const userSchema = new mongoose.Schema({
+username:{
+  type:String,
+  required:true,
+  unique:true,
+},
+password:{
+  type:String,
+},
+posts:[
+  {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'post'
+
+  }
+],
+dp:{
+  type:String,
+},
+email:{
+  type:String,
+  required:true,
+  unique:true,
+},
+
+fullname:{
+  type:String,
+  required:true,
+},
+
+});
+
+userSchema.plugin(plm);
+module.exports = mongoose.model('User', userSchema);
+
+
+
